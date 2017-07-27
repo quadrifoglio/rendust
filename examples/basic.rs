4fd6b09937adcf68e94da75221b21696dc7643f1
quadrifoglio <quadrifoglio.clement@protonmail.com>
2017-07-27 20:04:30 +0200

    added basic example

---
+++ examples/basic.rs
@@ -0,0 +1,12 @@
+extern crate rendust;
+
+use rendust::window::Window;
+
+fn main() {
+    let mut window = Window::new("Rendust example - Badic", 1280, 720).unwrap();
+
+    while !window.should_exit {
+        window.handle_events(|_| ());
+        window.swap_buffers();
+    }
+}
