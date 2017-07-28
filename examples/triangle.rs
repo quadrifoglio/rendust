bc5bc5ed8ae438db18f5d366f279617823fb9b49
quadrifoglio <quadrifoglio.clement@protonmail.com>
2017-07-28 16:03:02 +0200

    started mesh rendering

--- examples/triangle.rs
+++ examples/triangle.rs
@@ -1,10 +1,12 @@
 extern crate rendust;
 
+use rendust::Color;
 use rendust::window::Window;
 use rendust::shaders::Program;
+use rendust::mesh::{Vertex, Mesh};
 
 fn main() {
-    let mut window = Window::new("Rendust example - Shaders", 1280, 720, true).unwrap();
+    let mut window = Window::new("Rendust example - Triangle", 1280, 720, true).unwrap();
 
     let vert = r#"
         #version 140
@@ -26,15 +28,23 @@ fn main() {
         }
     "#;
 
-    rendust::set_clear_color(Color::new(0.0, 0.0, 0.0, 1.0));
-
     let program = Program::new(vert, frag).unwrap();
-    program.bind();
+
+    let triangle = Mesh::new(&[
+        Vertex{ position: [-1.0, -1.0, 0.0] },
+        Vertex{ position: [ 0.0,  1.0, 0.0] },
+        Vertex{ position: [ 1.0, -1.0, 0.0] },
+    ], None);
 
     while !window.should_exit {
         window.handle_events(|_| ());
 
+        rendust::set_clear_color(Color::new(0.0, 0.0, 0.0, 1.0));
         rendust::clear();
+
+        program.bind();
+        triangle.render();
+
         window.swap_buffers();
     }
 }
---
+++ src/mesh.rs
@@ -0,0 +1,77 @@
+use std;
+use std::os::raw::c_void;
+
+use gl;
+use gl::types::*;
+
+/// Represents a 3D vertex
+#[repr(C)]
+pub struct Vertex {
+    pub position: [GLfloat; 3]
+}
+
+impl Vertex {
+    /// Create a new vertex
+    pub fn new(pos: [GLfloat; 3]) -> Vertex {
+        Vertex {
+            position: pos
+        }
+    }
+}
+
+/// Represents a renderable 3D object
+pub struct Mesh {
+    vbo: GLuint
+}
+
+impl Mesh {
+    /// Create a new mesh
+    pub fn new(vertices: &[Vertex], indicies: Option<&[u32]>) -> Mesh {
+        unsafe {
+            // Create a VBO to store vertex data
+            let mut vbo: GLuint = 0;
+            gl::GenBuffers(1, (&mut vbo) as *mut GLuint);
+            gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+
+            // Upload the vertex data to the craphics card
+            gl::BufferData(
+                gl::ARRAY_BUFFER,
+                (std::mem::size_of::<Vertex>() * vertices.len()) as GLsizeiptr,
+                vertices.as_ptr() as *const c_void,
+                gl::STATIC_DRAW
+            );
+
+            Mesh{
+                vbo: vbo
+            }
+        }
+    }
+
+    /// Render the mesh to the screen
+    pub fn render(&self) {
+        unsafe {
+            // Bind the VBO
+            gl::BindBuffer(gl::ARRAY_BUFFER, self.vbo);
+
+            // Specify where each different attirbute of each vertex
+            // are in GPU memory
+
+            // Enable the position attribute
+            gl::EnableVertexAttribArray(0);
+
+            // Position attribute
+            // Offsert in vertex memory structure: 0
+            gl::VertexAttribPointer(
+                0 as GLuint,
+                3 as GLint,
+                gl::FLOAT,
+                gl::FALSE,
+                std::mem::size_of::<Vertex>() as GLsizei,
+                0 as *const c_void
+            );
+
+            // Render
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+        }
+    }
+}
