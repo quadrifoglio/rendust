cb203c2ea56d7ca824d2582bd48d15541211b308
quadrifoglio <quadrifoglio.clement@protonmail.com>
2017-07-29 00:46:40 +0200

    mesh: lines primitive & started scene example

---
+++ examples/scene.rs
@@ -0,0 +1,41 @@
+extern crate rendust;
+
+use rendust::math;
+use rendust::Color;
+use rendust::window::Window;
+use rendust::shaders::Program;
+use rendust::mesh::{Vertex, PrimitiveType, Mesh};
+
+fn main() {
+    let mut window = Window::new("Rendust example - Scene", 1280, 720, true).unwrap();
+
+    let program = Program::basic().unwrap();
+
+    let projection = math::perspective(90.0, 1280.0 / 720.0, 1.0, 1000.0);
+    program.set_uniform_matrix("projection", projection.as_ref());
+
+    let mut grid_vertices = Vec::new();
+    for x in -50..50 {
+        grid_vertices.push(Vertex::new([x as f32, -3.0, -50.0], Color::new(0.0, 0.0, 1.0, 1.0)));
+        grid_vertices.push(Vertex::new([x as f32, -3.0, 50.0], Color::new(0.0, 0.0, 1.0, 1.0)));
+    }
+
+    for z in -50..50 {
+        grid_vertices.push(Vertex::new([-50.0, -3.0, z as f32], Color::new(1.0, 0.0, 0.0, 1.0)));
+        grid_vertices.push(Vertex::new([50.0, -3.0, z as f32], Color::new(1.0, 0.0, 0.0, 1.0)));
+    }
+
+    let grid = Mesh::new(PrimitiveType::Lines, grid_vertices.as_ref(), None);
+
+    while !window.should_exit {
+        window.handle_events(|_| ());
+
+        rendust::set_clear_color(Color::new(0.0, 0.0, 0.0, 1.0));
+        rendust::clear();
+
+        program.bind();
+        grid.render();
+
+        window.swap_buffers();
+    }
+}
