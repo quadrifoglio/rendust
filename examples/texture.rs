c4c17bbaddcb82840cc427ae6c2295b5fc0a9831
quadrifoglio <quadrifoglio.clement@protonmail.com>
2017-07-29 18:32:57 +0200

    fixed examples

--- examples/texture.rs
+++ examples/texture.rs
@@ -1,15 +1,15 @@
 extern crate rendust;
 extern crate imagefmt;
 
+use rendust::Context;
 use rendust::window::Window;
-use rendust::shaders::Program;
 use rendust::mesh::{Vertex, Texture, PrimitiveType, Mesh};
 
 fn main() {
     let mut window = Window::new("Rendust example - Texture", 1280, 720, true).unwrap();
+    let _ = Context::new().unwrap();
 
     let image = imagefmt::read("examples/image.png", imagefmt::ColFmt::RGBA).unwrap();
-    let program = Program::basic().unwrap();
     let texture = Texture::new(image.w as u32, image.h as u32, image.buf.as_ref());
 
     let triangle = Mesh::new(PrimitiveType::Triangles, &[
@@ -24,9 +24,7 @@ fn main() {
         rendust::set_clear_color(0.0, 0.0, 0.0, 1.0);
         rendust::clear();
 
-        program.bind();
         texture.bind();
-
         triangle.render();
 
         window.swap_buffers();
--- examples/triangle.rs
+++ examples/triangle.rs
@@ -1,13 +1,14 @@
 extern crate rendust;
 
+use rendust::Context;
 use rendust::window::Window;
-use rendust::shaders::Program;
 use rendust::mesh::{Vertex, PrimitiveType, Mesh};
 
 fn main() {
     let mut window = Window::new("Rendust example - Triangle", 1280, 720, true).unwrap();
-
-    let program = Program::basic().unwrap();
+    
+    // A context must to created to initialize the shader program
+    let _ = Context::new().unwrap();
 
     let triangle = Mesh::new(PrimitiveType::Triangles, &[
         Vertex::new(-1.0, -1.0, 0.0).color(1.0, 0.0, 0.0, 1.0),
@@ -21,7 +22,6 @@ fn main() {
         rendust::set_clear_color(0.0, 0.0, 0.0, 1.0);
         rendust::clear();
 
-        program.bind();
         triangle.render();
 
         window.swap_buffers();
